bcc64196188d0576358954a0a990d1a7591ad89c
santius <santius1995@gmail.com>
2023-11-28 21:27:47 -0400

    added constants

---
+++ include/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONTSTANTS_H
+#define CONTSTANTS_H
+
+#define INDEX_EXT ".index"
+#define INDEX_EXT_LEN strlen(INDEX_EXT)
+#define INDEX_META_EXT ".index.meta"
+#define INDEX_META_EXT_LEN strlen(INDEX_META_EXT)
+
+#endif //CONTSTANTS_H
