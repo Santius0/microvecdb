54ad38ef138f39580f4769648172c9a3ebbfc9ba
santius <santius1995@gmail.com>
2023-12-06 00:38:25 -0400

    updated vector_collection

--- include/vector_collection.h
+++ include/vector_collection.h
@@ -12,12 +12,21 @@ namespace mvdb {
     class VectorCollection {
         std::unique_ptr<KvStore> kv_store_;
         std::unique_ptr<VectorIndex> vector_index_;
-        std::unique_ptr<Vectorizer> vectorizer;
+        std::unique_ptr<Vectorizer> vectorizer_;
         std::string name;
         std::string path;
+        uint64_t dimensions;
+        std::string model;
+
+        std::string kv_path;
+        std::string index_path;
+        std::string index_meta_path;
+        std::string meta_path;
     public:
-        VectorCollection(std::string name, std::string path);
+        VectorCollection(const std::string& name, const std::string& path, const uint64_t& dimensions,
+            const std::string& model);
         ~VectorCollection() = default;
+        static VectorCollection* load(const std::string& path);
         bool add(char* data, float* vector = nullptr);
         bool remove(uint64_t key);
         uint64_t* search(float* vectors, size_t num_vectors);
--- src/vector_collection.cpp
+++ src/vector_collection.cpp
@@ -1,68 +1,23 @@
-// #include "vector_collection.h"
-//
-// #include <string.h>
-// #include <stdlib.h>
-//
-//
-// vector_collection_t* vector_collection_init(const char* name, const char* path){
-//     vector_collection_t *collection = malloc(sizeof(vector_collection_t));
-//     if(!collection) return NULL;
-//
-//     collection->name = strdup(name);
-//     if(!collection->name) {
-//         vector_collection_free(collection);
-//         return NULL;
-//     }
-//     collection->path = strdup(path);
-//     if(!collection->path) {
-//         vector_collection_free(collection);
-//         return NULL;
-//     }
-//
-//     return collection;
-// }
-//
-// vector_collection_t* vector_collection_create(const char* name, const char* path, const bool overwrite) {
-//     vector_collection_t* collection = vector_collection_init(name, path);
-//
-//     // initialise kv_store
-//     collection->kv_store = kv_store_init(path, true, overwrite);
-//     if(!collection->kv_store) vector_collection_free(collection);
-//
-//     // initialise vector_store
-//     // place vector_store init code here
-//
-//     // if(!collection->vector_store) vector_collection_free(collection);
-//
-//     return collection;
-// }
-//
-// vector_collection_t* vector_collection_load(const char* name, const char* path) {
-//     vector_collection_t* collection = vector_collection_init(name, path);
-//
-//     // initialise kv_store
-//     collection->kv_store = kv_store_init(path, false, false);
-//     if(!collection->kv_store) vector_collection_free(collection);
-//
-//     // initialise vector_store
-//     // place vector_store init code here
-//
-//     // if(!collection->vector_store) vector_collection_free(collection);
-//
-//     return collection;
-// }
-//
-// void vector_collection_free(vector_collection_t* collection) {
-//     if(collection) {
-//         if(collection->name) free(collection->name);
-//         if(collection->path) free(collection->path);
-//         if(collection->kv_store) kv_store_free(collection->kv_store);
-//         free(collection);
-//         collection = NULL;
-//     }
-// }
-//
-// bool vector_collection_add(vector_collection_t* collection, const char* key, const float* vector, size_t vector_length) {
-//     return true;
-// }
-//
+#include "vector_collection.h"
+#include "constants.h"
+#include "utils.h"
+
+namespace mvdb {
+    VectorCollection::VectorCollection(const std::string& name, const std::string& path, const uint64_t& dimensions,
+        const std::string& model): name(name), path(trim(path)), dimensions(dimensions),
+    model(model) {
+        meta_path = path + "\\" + name + COLLECTION_META_EXT;
+        kv_path = path + "\\" + name + KV_STORE_EXT;
+        index_path = path + "\\" + name + INDEX_EXT;
+        index_meta_path = path + "\\" + name + INDEX_META_EXT;
+        vectorizer_ = std::make_unique<Vectorizer>(this->model, this->dimensions);
+        kv_store_ = std::make_unique<KvStore>(this->kv_path, false, false);
+        vector_index_ = std::make_unique<VectorIndex>(this->name, this->path, VectorIndexType::FLAT, this->dimensions);
+    }
+
+    VectorCollection* VectorCollection::load(const std::string& path) {
+        // return new VectorCollection();
+        return nullptr;
+    }
+
+}
