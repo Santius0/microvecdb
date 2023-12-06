a8fd7ee7f2a168deb4f5d27f73edc8a3e0b52db6
santius <santius1995@gmail.com>
2023-12-06 00:38:55 -0400

    updated constants

--- include/constants.h
+++ include/constants.h
@@ -2,6 +2,14 @@
 #define CONTSTANTS_H
 
 namespace mvdb {
+    // MicroVecDB
+    #define META_FILE_NAME "meta.json"
+    #define META_FILE_NAME_LEN strlen(META_FILE_NAME)
+
+    // Vector collection
+    #define COLLECTION_META_EXT ".meta"
+    #define COLLECTION_META_EXT_LEN strlen(COLLECTION_META_EXT)
+
     // Index store contants
     #define INDEX_EXT ".index"
     #define INDEX_EXT_LEN strlen(INDEX_EXT)
@@ -9,6 +17,8 @@ namespace mvdb {
     #define INDEX_META_EXT_LEN strlen(INDEX_META_EXT)
 
     // Data store contsants
+    #define KV_STORE_EXT ".data"
+    #define KV_STORE_EXT_LEN strlen(KV_STORE_EXT)
     #define MAX_KEY_CHARS 21
     #define MAX_KEY_SIZE_BYTES (sizeof(char) * MAX_KEY_CHARS) // (2^64 - 1) is a number with 20 digits
 }
