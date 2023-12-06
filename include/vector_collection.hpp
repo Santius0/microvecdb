300915d722aa760d13d51cfba52e89e860c7d6ef
santius <santius1995@gmail.com>
2023-12-06 16:16:51 -0400

    moved collection metadata

--- include/vector_collection.hpp
+++ include/vector_collection.hpp
@@ -4,32 +4,52 @@
 #include "kv_store.hpp"
 #include "vector_index.hpp"
 #include "vectorizer.hpp"
-
 #include <memory>
+#include <ostream>
 
 namespace mvdb {
+
+    class CollectionMetadata {
+        std::string name;
+        std::string indexFilePath;
+        std::string dataDirectoryPath;
+        size_t recordCount{};
+        size_t indexDimensions{};
+        std::string createdTimestamp;
+        std::string modifiedTimestamp;
+        void serialize(std::ostream& out) const;
+        void deserialize(std::istream& in);
+        friend class MetadataManager;
+    public:
+        CollectionMetadata() = default;
+        CollectionMetadata(std::string name, std::string indexFilePath, std::string dataDirectoryPath,
+            const size_t& recordCount, const size_t& indexDimensions);
+        ~CollectionMetadata() = default;
+        friend std::ostream& operator<<(std::ostream& os, const CollectionMetadata& obj) {
+            return os
+                   << "name: " << obj.name
+                   << " indexFilePath: " << obj.indexFilePath
+                   << " dataDirectoryPath: " << obj.dataDirectoryPath
+                   << " recordCount: " << obj.recordCount
+                   << " indexDimensions: " << obj.indexDimensions
+                   << " createdTimestamp: " << obj.createdTimestamp
+                   << " modifiedTimestamp: " << obj.modifiedTimestamp;
+        }
+    };
 
     class VectorCollection {
         std::unique_ptr<KvStore> kv_store_;
         std::unique_ptr<VectorIndex> vector_index_;
         std::unique_ptr<Vectorizer> vectorizer_;
-        std::string name;
-        std::string path;
-        uint64_t dimensions;
-        std::string model;
-
-        std::string kv_path;
-        std::string index_path;
-        std::string index_meta_path;
-        std::string meta_path;
     public:
-        VectorCollection(const std::string& name, const std::string& path, const uint64_t& dimensions,
-            const std::string& model);
+        VectorCollection() = default;
+        VectorCollection(const std::string& model, const uint64_t& dimensions, const std::string& path);
         ~VectorCollection() = default;
         static VectorCollection* load(const std::string& path);
-        bool add(char* data, float* vector = nullptr);
-        bool remove(uint64_t key);
-        uint64_t* search(float* vectors, size_t num_vectors);
+
+        // bool add(char* data, float* vector = nullptr);
+        // bool remove(uint64_t key);
+        // uint64_t* search(float* vectors, size_t num_vectors);
     };
 
 }
--- src/vector_collection.cpp
+++ src/vector_collection.cpp
@@ -1,20 +1,44 @@
 #include "vector_collection.hpp"
 #include "constants.hpp"
 #include "utils.hpp"
-
 #include <filesystem>
+#include <utility>
 
 namespace mvdb {
-    VectorCollection::VectorCollection(const std::string& name, const std::string& path, const uint64_t& dimensions,
-        const std::string& model): name(name), path(trim(path)), dimensions(dimensions),
-    model(model) {
-        meta_path = path + std::filesystem::path::preferred_separator + name + COLLECTION_META_EXT;
-        kv_path = path + std::filesystem::path::preferred_separator + name + KV_STORE_EXT;
-        index_path = path + std::filesystem::path::preferred_separator + name + INDEX_EXT;
-        index_meta_path = path + std::filesystem::path::preferred_separator + name + INDEX_META_EXT;
-        vectorizer_ = std::make_unique<Vectorizer>(this->model, this->dimensions);
-        kv_store_ = std::make_unique<KvStore>(this->kv_path, false, false);
-        vector_index_ = std::make_unique<VectorIndex>(this->name, this->path, VectorIndexType::FLAT, this->dimensions);
+
+    CollectionMetadata::CollectionMetadata(std::string name, std::string indexFilePath, std::string dataDirectoryPath,
+            const size_t& recordCount, const size_t& indexDimensions): name(std::move(name)),
+    indexFilePath(std::move(indexFilePath)), dataDirectoryPath(std::move(dataDirectoryPath)),
+    recordCount(recordCount), indexDimensions(indexDimensions) {
+        createdTimestamp = getCurrentTimeStamp();
+        modifiedTimestamp = createdTimestamp;
+    }
+
+    void CollectionMetadata::serialize(std::ostream& out) const {
+        serializeString(out, name);
+        serializeString(out, indexFilePath);
+        serializeString(out, dataDirectoryPath);
+        serializeSizeT(out, recordCount);
+        serializeSizeT(out, indexDimensions);
+        serializeString(out, createdTimestamp);
+        serializeString(out, modifiedTimestamp);
+    }
+
+    void CollectionMetadata::deserialize(std::istream& in) {
+        name = deserializeString(in);
+        indexFilePath = deserializeString(in);
+        dataDirectoryPath = deserializeString(in);
+        recordCount = deserializeSizeT(in);
+        indexDimensions = deserializeSizeT(in);
+        createdTimestamp = deserializeString(in);
+        modifiedTimestamp = deserializeString(in);
+    }
+
+    VectorCollection::VectorCollection(const std::string& model, const uint64_t& dimensions, const std::string& path) {
+        if(!std::filesystem::exists(path)) std::filesystem::create_directory(path);
+        // vectorizer_ = std::make_unique<Vectorizer>(model, dimensions);
+        // kv_store_ = std::make_unique<KvStore>(this->kv_path, false, false);
+        // vector_index_ = std::make_unique<VectorIndex>(this->name, this->path, VectorIndexType::FLAT, this->dimensions);
     }
 
     VectorCollection* VectorCollection::load(const std::string& path) {
