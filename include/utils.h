c86956d88ed2c2af93ecce270d029cf1ef135bf2
santius <santius1995@gmail.com>
2023-12-06 00:36:41 -0400

    added utils

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+namespace mvdb {
+
+    std::string trim(const std::string& str);
+
+}
+
+#endif //UTILS_H
---
+++ src/utils.cpp
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+namespace mvdb {
+
+    std::string trim(const std::string& str) {
+        const std::string chars = " \t\n\r\f\v/\\";
+        const size_t start = str.find_first_not_of(chars);
+        if (start == std::string::npos) return ""; // String contains only whitespace or specified characters
+        const size_t end = str.find_last_not_of(chars);
+        return str.substr(start, end - start + 1);
+    }
+
+}
