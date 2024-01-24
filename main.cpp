cef2c7e08ebbd903b926ecbf01b7f37fabf9c541
santius <santius1995@gmail.com>
2024-01-24 17:47:35 -0400

    initialized server

--- main.cpp
+++ main.cpp
@@ -5,37 +5,7 @@
 //#include <thread>
 //#include <db.h>
 
-//void server_thread() {
-//    zmq::context_t context(1);
-//    zmq::socket_t socket(context, ZMQ_REP);
-//    socket.bind("inproc://example");
-//
-//    while (true) {
-//        zmq::message_t request;
-//        socket.recv(&request);
-//        std::string recv_msg(static_cast<char*>(request.data()), request.size());
-//        std::cout << "Server received: " << recv_msg << std::endl;
-//
-//        zmq::message_t reply(5);
-//        memcpy(reply.data(), "World", 5);
-//        socket.send(reply);
-//    }
-//}
-//
-//void client_thread(const std::string& server_address) {
-//    zmq::context_t context(1);
-//    zmq::socket_t socket(context, ZMQ_REQ);
-//    socket.connect(server_address); // Connect to the server address
-//
-//    zmq::message_t request(5);
-//    memcpy(request.data(), "Hello", 5);
-//    socket.send(request);
-//
-//    zmq::message_t reply;
-//    socket.recv(&reply);
-//    std::string recv_msg(static_cast<char*>(reply.data()), reply.size());
-//    std::cout << "Client received: " << recv_msg << std::endl;
-//}
+
 
 // device #1: ./microvecdb_main tcp://192.168.1.11:5555
 // device #1: ./microvecdb_main tcp://192.168.1.10:5555
