a69c5dc84dd25878426db9f3d9533bfa217f0de2
santius0 <santius1995@gmail.com>
2024-05-13 08:51:18 -0400

    arm

--- jetson/python/setup.py
+++ jetson/python/setup.py
@@ -12,19 +12,19 @@ import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
 
-module = Extension("pymicrovecdb",
+module = Extension("microvecdb",
                    sources = [f"{BASE_DIR}/python/pymicrovecdb.cpp"],
                    include_dirs=[f"{BASE_DIR}/include/core", f"{BASE_DIR}/include/index", f"{BASE_DIR}/include/operators",
                                  f"{BASE_DIR}/include/storage", f"{BASE_DIR}/faiss/include", f"{BASE_DIR}/annoy",
                                  f"{BASE_DIR}/SPTAG/AnnService", f"{BASE_DIR}/SPTAG/AnnService/inc",
                                  f"{BASE_DIR}/numpy/include"],
-                   library_dirs=[f"{BASE_DIR}/lib"],
-                   libraries=['microvecdb'],
-                   extra_compile_args=['-std=c++17 -fopenmp -fPIC']
+                   library_dirs=[f"{BASE_DIR}/lib", f"{BASE_DIR}/SPTAG/Release"],
+                   libraries=['microvecdb', 'DistanceUtils', 'SPTAGLibStatic', 'pthread'],
+                   extra_compile_args=['-std=c++17', '-fopenmp', '-fPIC']
                    )
 
 setup(
-    name='pymicrovecdb',
+    name='microvecdb',
     version='0.1.0',
     description='MicroVecDB C++ Interface Package',
     ext_modules=[module],
