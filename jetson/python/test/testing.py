8e550c6a70615e726247e2f3f5132e50b708237a
santius0 <santius1995@gmail.com>
2024-05-13 10:59:37 -0400

    arm

--- jetson/python/test/testing.py
+++ jetson/python/test/testing.py
@@ -1,9 +1,10 @@
 import sys
-sys.path.append("/usr/lib/python3.6")
-sys.path.append("/usr/lib/python3")
+# sys.path.append("/usr/lib/python3.6")
+sys.path.append("/usr/lib/python3/dist-packages")
 
 print(sys.path)
 
+import numpy as np
 import microvecdb as mvdb_c
 
 mvdb_c.hello(3)
