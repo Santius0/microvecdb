37efc418c66eea3b8259a83407bc51d9a768db07
santius0 <santius1995@gmail.com>
2024-05-14 16:11:42 -0400

    big

--- jetson/create_sysroot_on_device.sh
+++ jetson/create_sysroot_on_device.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# Define variables
-DEST_DIR="/media/67C9-117A/jetson-nano-sysroot"  # Destination directory on the local machine
-
-# Create the destination directory if it does not already exist
-mkdir -p "$DEST_DIR"
-
 # List of directories to copy
 declare -a directories=(
-    "/lib"
-    "/usr/lib"
-    "/usr/include"
-    "/bin"
-    "/sbin"
-    "/etc"
-    "/opt"
-    "/usr/local"
-    "/usr/bin"
-    "/usr/sbin"
+  "/lib"
+  "/usr/lib"
+  "/usr/include"
+  "/bin"
+  "/sbin"
+  "/etc"
+  "/opt"
+  "/usr/local"
+  "/usr/bin"
+  "/usr/sbin"
 )
 
-# Loop through directories and copy each one
+# Destination directory on the flash drive
+destination="/media/67C9-117A/jetson-nano-sysroot"
+
+# Loop through each directory and copy it using rsync
 for dir in "${directories[@]}"; do
-    # Check if the source directory exists
-    if [ ! -d "$dir" ]; then
-        echo "Directory $dir does not exist."
-        continue
-    fi
-    # Define destination path for current directory
-    dest_path="$DEST_DIR$(dirname $dir)"
-    # Create destination directory if it does not exist
-    mkdir -p "$dest_path"
-    # Use cp command to copy directory contents
-    cp -a "$dir/." "$dest_path/"
+  echo "Copying $dir to $destination"
+  rsync -av --exclude='*.lock' --exclude='/var/run/' "$dir/" "$destination$(basename $dir)/"
 done
 
-echo "Copying complete."
+echo "All directories have been copied."
