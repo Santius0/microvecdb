bd5ce07a224ea60c86ebc3ffd12d29625dff7123
santius0 <santius1995@gmail.com>
2024-05-15 10:43:52 -0400

    adjustment

--- jetson/create_sysroot_on_device.sh
+++ jetson/create_sysroot_on_device.sh
@@ -17,10 +17,13 @@ declare -a directories=(
 # Destination directory on the flash drive
 destination="/media/67C9-117A/jetson-nano-sysroot"
 
+mkdir -p "$destination"
+
 # Loop through each directory and copy it using rsync
 for dir in "${directories[@]}"; do
-  echo "Copying $dir to $destination"
-  rsync -av --exclude='*.lock' --exclude='/var/run/' "$dir/" "$destination$(basename $dir)/"
+  echo "Copying $dir to $destination$(dirname $dir)"
+  mkdir -p "$destination$(dirname $dir)"
+  rsync -avzHAX --exclude='*.lock' --exclude='/var/run/' "$dir" "$destination$(dirname $dir)"
 done
 
 echo "All directories have been copied."
