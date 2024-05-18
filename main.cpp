46bb6fa6d8ab0129f6ebb9949da81bdbb425ea01
santius0 <santius1995@gmail.com>
2024-05-17 22:29:44 -0400

    testing

--- main.cpp
+++ main.cpp
@@ -3,5 +3,15 @@
 int main(){
     auto *db = new mvdb::MVDB<float>();
     delete db;
+    int a = 1, b = 2;
+#ifdef FAISS
+    if(a == 1){
+        std::cout << "1";
+    }
+    else
+#endif
+    if(a == 1){
+            std::cout << "dd";
+    }
     return 0;
 }
