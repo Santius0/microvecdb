975800e5466745c7e4f4046772afdcc0ab8aaae0
santius0 <santius1995@gmail.com>
2024-07-21 10:05:57 -0400

    changes

--- python/case_study/facial_recognition.py
+++ python/case_study/facial_recognition.py
@@ -3,6 +3,11 @@
 # pip install Pillow
 # pip install numpy
 
+# pip install torch
+# pip install facenet-pytorch
+# pip install Pillow
+# pip install numpy
+
 import os
 import random
 import shutil
@@ -15,12 +20,13 @@ import pickle
 import time
 import csv
 
-# initialize face detection and recognition models
+# Initialize face detection and recognition models
 mtcnn = MTCNN(image_size=160, margin=0)
 model = InceptionResnetV1(pretrained='vggface2').eval()
 
 DB_PATH = './facesDB'
 FACE_DIR = './lfw/lfw-deepfunneled/lfw-deepfunneled'  # LFW dataset
+TEST_IMAGES_PATH = './test_images.pkl'
 
 spann_index_params = {
     'build_config_path': "./buildconfig.ini",
@@ -37,6 +43,7 @@ spann_index_params = {
     'batch_size': 2000,
     'thread_num': 10,
 }
+
 
 def preprocess_image(image_path):
     img = Image.open(image_path)
@@ -55,49 +62,56 @@ def get_embedding(image_path):
     else:
         return None
 
-
-embeddings = []
-binary_data = []
-test_images = []
-
-for idx, person in enumerate(os.listdir(FACE_DIR)):
-    person_dir = os.path.join(FACE_DIR, person)
-    if os.path.isdir(person_dir):
-        images = [os.path.join(person_dir, img) for img in os.listdir(person_dir)]
-        if images:
-            selected_image = random.choice(images)
-            test_images.extend(img for img in images if img != selected_image)
-
-            print(f"{idx} => Generating embedding for {selected_image}...")
-            embedding = get_embedding(selected_image)
-            if embedding is not None:
-                embeddings.append(embedding)
-                with open(selected_image, 'rb') as img_file:
-                    binary_image = img_file.read()
-                person_data = {
-                    "name": person,
-                    "image_path": selected_image,
-                    "image": binary_image
-                }
-                binary_data.append(pickle.dumps(person_data))
-            print(f"{idx} => {selected_image} embedding generated.")
-
-embeddings = np.vstack(embeddings)
-binary_data = np.array(binary_data, dtype=object)
-
 face_db = mvdb.MVDB(dtype=mvdb.DataType.FLOAT32)
 
-if os.path.exists(DB_PATH) and os.path.isdir(DB_PATH):
-    shutil.rmtree(DB_PATH)
+if not os.path.exists(DB_PATH):
+    embeddings = []
+    binary_data = []
+    test_images = []
 
-face_db.create(
-    index_type=mvdb.IndexType.SPANN,
-    dims=512,
-    path=DB_PATH,
-    initial_data=embeddings,
-    initial_objs=binary_data,
-    **spann_index_params,
-)
+    for idx, person in enumerate(os.listdir(FACE_DIR)):
+        person_dir = os.path.join(FACE_DIR, person)
+        if os.path.isdir(person_dir):
+            images = [os.path.join(person_dir, img) for img in os.listdir(person_dir)]
+            if images:
+                selected_image = random.choice(images)
+                test_images.extend(img for img in images if img != selected_image)
+
+                print(f"{idx} => Generating embedding for {selected_image}...")
+                embedding = get_embedding(selected_image)
+                if embedding is not None:
+                    embeddings.append(embedding)
+                    with open(selected_image, 'rb') as img_file:
+                        binary_image = img_file.read()
+                    person_data = {
+                        "name": person,
+                        "image_path": selected_image,
+                        "image": binary_image
+                    }
+                    binary_data.append(pickle.dumps(person_data))
+                print(f"{idx} => {selected_image} embedding generated.")
+
+    embeddings = np.vstack(embeddings)
+    binary_data = np.array(binary_data, dtype=object)
+
+    face_db.create(
+        index_type=mvdb.IndexType.SPANN,
+        dims=512,
+        path=DB_PATH,
+        initial_data=embeddings,
+        initial_objs=binary_data,
+        **spann_index_params,
+    )
+
+    # save test images
+    with open(TEST_IMAGES_PATH, 'wb') as f:
+        pickle.dump(test_images, f)
+else:
+    face_db.open(DB_PATH)
+
+    with open(TEST_IMAGES_PATH, 'rb') as f:
+        test_images = pickle.load(f)
+
 
 def validate_performance(test_images_):
     total_latency = 0
@@ -133,9 +147,6 @@ def validate_performance(test_images_):
             retrieved_data = pickle.loads(data)
             retrieved_path = retrieved_data["image_path"]
             retrieved_name = retrieved_data["name"]
-            # image_stream = io.BytesIO(retrieved_data["image"])
-            # image = Image.open(image_stream)
-            # image.save(f'./retrieved/{retrieved_data["name"]}.png')
             if retrieved_data["name"] in image_path:
                 recall_at_1 = 1
             total_recall += recall_at_1
@@ -166,3 +177,7 @@ def validate_performance(test_images_):
 avg_latency, avg_recall = validate_performance(test_images)
 print(f'Avg Retrieval Time (s): {avg_latency}')
 print(f'Avg Recall@1: {avg_recall}')
+
+# image_stream = io.BytesIO(retrieved_data["image"])
+# image = Image.open(image_stream)
+# image.save(f'./retrieved/{retrieved_data["name"]}.png')
